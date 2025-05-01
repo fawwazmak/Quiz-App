f8d27186c03871c88102f0cea64c438c394a57de
fawwazmak <fawwazmakinde47@gmail.com>
2025-05-01 15:54:46 +0100

    Done with each section save functionalities

--- script/index.js
+++ script/index.js
@@ -1,10 +1,46 @@
 let questions = [
-    {shape: "square", question: "Who is the main actor of demon slayer?", options: ["Tanjiro", "Yagami", "inosuke", "Zenitsu", "hashira"]},
-    {shape: "square", question: "Who is the main actor of Death note?", options: ["Tanjiro", "Yagami", "inosuke", "Zenitsu", "hashira"]},
-    {shape: "square", question: "Who is the main actor of demon slayer?", options: ["Tanjiro", "Yagami", "inosuke", "Zenitsu", "hashira"]},
-    {shape: "square", question: "Who is the main actor of demon slayer?", options: ["Tanjiro", "Yagami", "inosuke", "Zenitsu", "hashira"]},
-    {shape: "square", question: "Who is the main actor of demon slayer?", options: ["Tanjiro", "Yagami", "inosuke", "Zenitsu", "hashira"]},
-    {shape: "square", question: "Who is the main actor of demon slayer?", options: ["Tanjiro", "Yagami", "inosuke", "Zenitsu", "hashira"]},
+    {shape: "square", question: "Who is the main actor of demon slayer?", options: [
+        {optn: "Tanjiro", status: "wrong"},
+        {optn: "Ramen", status: "right"},
+        {optn: "Yagami", status: "wrong"},
+        {optn: "inosuke", status: "wrong"},
+        {optn: "hashira", status: "wrong"},
+    ]},
+    {shape: "square", question: "Who is the main actor of Death note?", options: [
+        {optn: "Tanjiro", status: "wrong"},
+        {optn: "Ramen", status: "right"},
+        {optn: "Yagami", status: "wrong"},
+        {optn: "inosuke", status: "wrong"},
+        {optn: "hashira", status: "wrong"},
+    ]},
+    {shape: "square", question: "Which state is the capital of Nigeria?", options: [
+        {optn: "Tanjiro", status: "wrong"},
+        {optn: "Ramen", status: "right"},
+        {optn: "Yagami", status: "wrong"},
+        {optn: "inosuke", status: "wrong"},
+        {optn: "hashira", status: "wrong"},
+    ]},
+    {shape: "square", question: "When was Prophet Muhammad given birth to?", options: [
+        {optn: "Tanjiro", status: "wrong"},
+        {optn: "Ramen", status: "right"},
+        {optn: "Yagami", status: "wrong"},
+        {optn: "inosuke", status: "wrong"},
+        {optn: "hashira", status: "wrong"},
+    ]},
+    {shape: "square", question: "What was Naruto's favorite food?", options: [
+        {optn: "Tanjiro", status: "wrong"},
+        {optn: "Ramen", status: "right"},
+        {optn: "Yagami", status: "wrong"},
+        {optn: "inosuke", status: "wrong"},
+        {optn: "hashira", status: "wrong"},
+    ]},
+    {shape: "square", question: "Who is the main actor of demon slayer?", options: [
+        {optn: "Tanjiro", status: "wrong"},
+        {optn: "Ramen", status: "right"},
+        {optn: "Yagami", status: "wrong"},
+        {optn: "inosuke", status: "wrong"},
+        {optn: "hashira", status: "wrong"},
+    ]},
 ];
 
 let questionSpace = document.getElementById("question-container");
@@ -13,28 +49,13 @@ let questionSpace = document.getElementById("question-container");
 for (let i = 0; i < questions.length; i++) {
     const title = document.createElement('h1');
     const optionsContainer = document.createElement("div");
-    let options = "";
-    
-    for (let y = 0; y < questions[i].options.length; y++) {
-        
-    }
-
-    optionsContainer.innerHTML = `
-        <div class="option">
-            <p>Hello</p>
-        </div>
-    `
-
-    // for (let y = 0; y < questions[i].options.length; y++) {
-    //     const eachOption = options[y];
-    //     optionsContainer.appendChild(eachOption)
-    //     console.log(i, y)
-    // }
+    let options = questions[i].options.map(option => `<div class="option"><p>${option.optn}</p></div>`).join('');
+    optionsContainer.innerHTML = `${options}`;
 
     title.textContent = questions[i].question;
     title.className = 'child';
     questionSpace.appendChild(title);
 
     questionSpace.appendChild(optionsContainer);
-    optionsContainer.className = "options"
+    optionsContainer.id = "options"
 }
